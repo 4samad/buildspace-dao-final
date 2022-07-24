de32e3780275d5a83b3405de6cc3c4621823c48f
AbdulSamad <47020656+4samad@users.noreply.github.com>
2022-07-24 11:59:23 +0400

    create script revoke roles

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const token = sdk.getToken("0xc3bD8ba8821c805f7112f4b881A1CE3de1e40b0a");
+
+(async () => {
+  try {
+    // Log the current roles.
+    const allRoles = await token.roles.getAll();
+
+    console.log("👀 Roles that exist right now:", allRoles);
+
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    await token.roles.setAll({ admin: [], minter: [] });
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await token.roles.getAll()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
